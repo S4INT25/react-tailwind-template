06aeeb92b82a1e3da6c7c711d8cbaecde43d5848
saint <lucksonmwanambulo25@gmail.com>
2021-07-28 20:51:34 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
+    <div className="flex justify-center items-center h-screen w-full">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="text-6xl bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500 ">
+          React With Tailwind Startup
+        </h1>
       </header>
     </div>
   );
